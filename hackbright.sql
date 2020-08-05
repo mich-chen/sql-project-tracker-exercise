d9941fda931eb21da7a3dc05ea49a3b094bae570
Michelle Chen <mich.chen.94@gmail.com>
2020-08-04 17:40:56 -0700

    linked tables together

--- hackbright.sql
+++ hackbright.sql
@@ -35,6 +35,42 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
+-- Name: grades; Type: TABLE; Schema: public; Owner: vagrant
+--
+
+CREATE TABLE public.grades (
+    id integer NOT NULL,
+    student_github character varying(50),
+    project_title character varying(50),
+    grade integer
+);
+
+
+ALTER TABLE public.grades OWNER TO vagrant;
+
+--
+-- Name: grades_id_seq; Type: SEQUENCE; Schema: public; Owner: vagrant
+--
+
+CREATE SEQUENCE public.grades_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.grades_id_seq OWNER TO vagrant;
+
+--
+-- Name: grades_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: vagrant
+--
+
+ALTER SEQUENCE public.grades_id_seq OWNED BY public.grades.id;
+
+
+--
 -- Name: projects; Type: TABLE; Schema: public; Owner: vagrant
 --
 
@@ -107,6 +143,13 @@ ALTER SEQUENCE public.students_id_seq OWNED BY public.students.id;
 
 
 --
+-- Name: grades id; Type: DEFAULT; Schema: public; Owner: vagrant
+--
+
+ALTER TABLE ONLY public.grades ALTER COLUMN id SET DEFAULT nextval('public.grades_id_seq'::regclass);
+
+
+--
 -- Name: projects id; Type: DEFAULT; Schema: public; Owner: vagrant
 --
 
@@ -121,11 +164,30 @@ ALTER TABLE ONLY public.students ALTER COLUMN id SET DEFAULT nextval('public.stu
 
 
 --
+-- Data for Name: grades; Type: TABLE DATA; Schema: public; Owner: vagrant
+--
+
+COPY public.grades (id, student_github, project_title, grade) FROM stdin;
+1	jhacks	Markov	10
+2	jhacks	Blockly	2
+3	sdevelops	Markov	50
+4	sdevelops	Blockly	100
+5	jhacks	shopping	60
+6	sdevelops	Claculator	40
+\.
+
+
+--
 -- Data for Name: projects; Type: TABLE DATA; Schema: public; Owner: vagrant
 --
 
 COPY public.projects (id, title, description, max_grade) FROM stdin;
 1	Markov	Tweets generated from Markov chains	50
+2	Blockly	Programmatic Logic Puzzle Game	100
+3	Calculator	calculate stuff	80
+4	Sharkwords	web version of hangman	30
+5	Cards	use React to make trading cards	50
+6	shopping	making a shopping site for melons	20
 \.
 
 
@@ -140,10 +202,17 @@ COPY public.students (id, first_name, last_name, github) FROM stdin;
 
 
 --
+-- Name: grades_id_seq; Type: SEQUENCE SET; Schema: public; Owner: vagrant
+--
+
+SELECT pg_catalog.setval('public.grades_id_seq', 6, true);
+
+
+--
 -- Name: projects_id_seq; Type: SEQUENCE SET; Schema: public; Owner: vagrant
 --
 
-SELECT pg_catalog.setval('public.projects_id_seq', 1, true);
+SELECT pg_catalog.setval('public.projects_id_seq', 6, true);
 
 
 --
@@ -151,6 +220,14 @@ SELECT pg_catalog.setval('public.projects_id_seq', 1, true);
 --
 
 SELECT pg_catalog.setval('public.students_id_seq', 2, true);
+
+
+--
+-- Name: grades grades_pkey; Type: CONSTRAINT; Schema: public; Owner: vagrant
+--
+
+ALTER TABLE ONLY public.grades
+    ADD CONSTRAINT grades_pkey PRIMARY KEY (id);
 
 
 --
